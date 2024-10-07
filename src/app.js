23cd4548a758d0213b483dbcf35b2aedfa27ab87
Uma Mahesh <adabala.um@gmail.com>
2024-10-07 22:01:09 +0530

    Playing with routing..

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello!!");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Umesh", lastName: "Adabala" });
+});
+app.post("/user", (req, res) => {
+  res.send("Data posted successfully!!");
+});
+app.delete("/user", (req, res) => {
+  res.send("Data deleted!!");
 });
 
-app.get("/test", (req, res) => {
+app.get("/test/:testid/result/:resultid", (req, res) => {
+  console.log(req.params);
   res.send("Test the server!!");
 });
 
-app.get("/home", (req, res) => {
-  res.send("Home!!");
-});
+// app.get("/home", (req, res) => {
+//   res.send("Home!!");
+// });
 
 app.listen(7777, () => {
   console.log("Server is running!!!");
